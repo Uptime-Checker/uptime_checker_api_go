e0a46be0248db7ec3f0b51d6f86afac2629422fa
Hasan <mr.k779@outlook.com>
2023-03-02 04:18:24 +0600

    Update user.go

--- web/controller/user.go
+++ web/controller/user.go
@@ -2,16 +2,23 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/Uptime-Checker/uptime_checker_api_go/domain"
 )
 
 type UserController struct {
+	userDomain *domain.UserDomain
+}
+
+func NewUserController(userDomain *domain.UserDomain) *UserController {
+	return &UserController{userDomain: userDomain}
 }
 
 type GuestUserBody struct {
 	Email string `json:"email" validate:"required,email,min=6,max=32"`
 }
 
-func CreateGuestUser(c *fiber.Ctx) error {
+func (u *UserController) CreateGuestUser(c *fiber.Ctx) error {
 	body := new(GuestUserBody)
 
 	if err := c.BodyParser(body); err != nil {
