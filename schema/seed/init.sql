d5936e5b11eae259562940add54a7aec437f3164
Hasan <mr.k779@outlook.com>
2023-03-06 22:12:30 +0600

    Create init.sql

---
+++ schema/seed/init.sql
@@ -0,0 +1,52 @@
+-- Insert Regions
+
+INSERT INTO region (name, key) VALUES('Sunnyvale, California (US)','sjc');
+INSERT INTO region (name, key) VALUES('Frankfurt, Germany','fra');
+INSERT INTO region (name, key) VALUES('Singapore','sin');
+INSERT INTO region (name, key) VALUES('Sydney, Australia','syd');
+INSERT INTO region (name, key) VALUES('Toronto, Canada','yyz');
+
+-- Insert Roles
+
+INSERT INTO role (name, type) VALUES('Super Admin',1);
+INSERT INTO role (name, type) VALUES('Admin',2);
+INSERT INTO role (name, type) VALUES('Editor',3);
+INSERT INTO role (name, type) VALUES('Member',4);
+
+-- Insert Products
+
+INSERT INTO product (name, description, tier) VALUES('Free','Free for lifetime',1);
+
+-- Insert Features
+
+INSERT INTO feature (name, type) VALUES('API_CHECK_COUNT',1);
+INSERT INTO feature (name, type) VALUES('API_CHECK_INTERVAL',1);
+INSERT INTO feature (name, type) VALUES('USER_COUNT',10);
+
+-- Insert Plans
+
+INSERT INTO plan (price, type, product_id) VALUES(0,1,1); -- Free/Monthly
+
+-- Insert Product Features
+
+INSERT INTO product_feature (count, feature_id, product_id) VALUES(5,1,1);    -- Free/API_CHECK_COUNT/5
+INSERT INTO product_feature (count, feature_id, product_id) VALUES(300,2,1);  -- Free/API_CHECK_INTERVAL/300
+INSERT INTO product_feature (count, feature_id, product_id) VALUES(1,3,1);    -- Free/USER_COUNT/1
+
+-- Insert Role Claims
+
+INSERT INTO role_claim (claim, role_id) VALUES('CREATE_RESOURCE',3); -- Editor
+INSERT INTO role_claim (claim, role_id) VALUES('UPDATE_RESOURCE',3);
+INSERT INTO role_claim (claim, role_id) VALUES('DELETE_RESOURCE',3);
+
+INSERT INTO role_claim (claim, role_id) VALUES('CREATE_RESOURCE',2); -- Admin
+INSERT INTO role_claim (claim, role_id) VALUES('UPDATE_RESOURCE',2);
+INSERT INTO role_claim (claim, role_id) VALUES('DELETE_RESOURCE',2);
+INSERT INTO role_claim (claim, role_id) VALUES('INVITE_USER',2);
+
+INSERT INTO role_claim (claim, role_id) VALUES('CREATE_RESOURCE',1); -- Super Admin
+INSERT INTO role_claim (claim, role_id) VALUES('UPDATE_RESOURCE',1);
+INSERT INTO role_claim (claim, role_id) VALUES('DELETE_RESOURCE',1);
+INSERT INTO role_claim (claim, role_id) VALUES('INVITE_USER',1);
+INSERT INTO role_claim (claim, role_id) VALUES('BILLING',1);
+INSERT INTO role_claim (claim, role_id) VALUES('DESTROY_ORGANIZATION',1);
