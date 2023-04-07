5ac5ccdd380fe8c20b677ffe2796868be6dfe327
Hasan <mr.k779@outlook.com>
2023-04-07 21:11:35 +0600

    Update alarm_policy.go

--- service/alarm_policy.go
+++ service/alarm_policy.go
@@ -1,6 +1,11 @@
 package service
 
-import "github.com/Uptime-Checker/uptime_checker_api_go/domain"
+import (
+	"context"
+
+	"github.com/Uptime-Checker/uptime_checker_api_go/domain"
+	"github.com/Uptime-Checker/uptime_checker_api_go/schema/uptime_checker/public/model"
+)
 
 type AlarmPolicyService struct {
 	alarmPolicyDomain *domain.AlarmPolicyDomain
@@ -8,4 +13,14 @@ type AlarmPolicyService struct {
 
 func NewAlarmPolicyService(alarmPolicyDomain *domain.AlarmPolicyDomain) *AlarmPolicyService {
 	return &AlarmPolicyService{alarmPolicyDomain: alarmPolicyDomain}
+}
+
+func (a *AlarmPolicyService) Get(
+	ctx context.Context, monitorID, organizationID int64,
+) (*model.AlarmPolicy, error) {
+	alarmPolicy, err := a.alarmPolicyDomain.GetMonitorAlarmPolicy(ctx, organizationID, monitorID)
+	if err != nil {
+		return a.alarmPolicyDomain.GetGlobal(ctx, organizationID)
+	}
+	return alarmPolicy, nil
 }
