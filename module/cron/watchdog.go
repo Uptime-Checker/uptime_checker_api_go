9d125420cfde9bda8823b94f6c54c9fe3e450d9b
Hasan <mr.k779@outlook.com>
2023-05-04 18:52:50 +0600

    Update watchdog.go

--- module/cron/watchdog.go
+++ module/cron/watchdog.go
@@ -82,7 +82,7 @@ func (c *Cron) watchTheDog(ctx context.Context, tid string) error {
 
 func (c *Cron) stopTheDog(ctx context.Context) {
 	tid := pkg.GetTracingID(ctx)
-	lgr.Print(tid, 1, "stopping the dog in", config.Region.Name)
+	lgr.Print(tid, 1, "stopping the dog")
 	watchDogValue := c.propertyService.Get(ctx, resource.PropertyKeyWatchDog)
 	if watchDogValue == nil {
 		runWatchDog, err := strconv.ParseBool(*watchDogValue)
