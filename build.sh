6fed38a9d85ec01fd4a19967e1f6e67a3b81f7d1
Hasan <mr.k779@outlook.com>
2023-08-02 15:00:13 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-set -e
-
-PROJ="uptime_checker_api_go"
-
 if ! [ -x "$(command -v go)" ]; then
     echo "go is not installed"
     exit 1
@@ -17,23 +13,9 @@ if [ -z "${GOPATH}" ]; then
     exit 1
 fi
 
-COMMIT=$(git rev-parse --short HEAD)
-TAG=$(git describe --exact-match --abbrev=0 --tags "${COMMIT}" 2> /dev/null || true)
-
-if [ -z "${TAG}" ]; then
-    VERSION=${COMMIT}
-else
-    VERSION=${TAG}
-fi
-
-if [ -n "$(git diff --shortstat 2> /dev/null | tail -n1)" ]; then
-    VERSION="${VERSION}-dirty"
-fi
-
-
 go mod verify
 go mod vendor
 go mod tidy
 
 ./format.sh
-go build -o bin/${PROJ}
+go build -o bin
